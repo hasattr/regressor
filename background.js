db8be71778f134e5489642fb47993f158c9d4bde
Ian Jung <jungh@live.com>
2020-10-28 14:03:14 +0000

    Fix functinality on visual tab and enable add button

--- background.js
+++ background.js
@@ -1,5 +1,19 @@
 chrome.runtime.onInstalled.addListener(function() {
   
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'iwjira.activision.com'},
+        }),
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostEquals: 'tajira.activision.com'},
+        })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+  
   const regressType = ["VF", "VO", "CNV", "CNR"]
   
   regressType.forEach((type) => {
--- contentScript.js
+++ contentScript.js
@@ -1,19 +1,21 @@
 
 
 //content script
-let textArea = null
-document.addEventListener("contextmenu", function(event){
-  
+let textArea;
+
+document.addEventListener("contextmenu", function(event){  
   textArea = event.target
-  console.log("context", event.target)
-}, true);
+});
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-    if(message.status === "addComment") {
-      if(textArea.tagName === 'TEXTAREA'&& textArea.id === 'comment') textArea.value = message.message
+  if(message.status === "addComment" && textArea) {
+      console.log("sender", sender)
+      let add = window.parent.document.querySelector("input[disabled='disabled']")
+      add && add.removeAttribute("disabled")
+      if(textArea.tagName === 'TEXTAREA' && textArea.id === 'comment') textArea.value = message.message
       if(textArea.tagName === "HTML") textArea.querySelector('p').innerHTML = message.message
       if(textArea.tagName === "P") textArea.innerHTML = message.message
-
+     
     } 
 
     if (message.status === "missing") {
